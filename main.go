30a2172d618c52f65b81103d678fab97f9a6178d
Piotr Prokopowicz <p.prokopowicz1@gmail.com>
2023-08-03 16:15:16 +0200

    Not found handling added

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
