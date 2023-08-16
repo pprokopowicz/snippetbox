e2461482d51aafb0f0f39b5b57be33480cc99a6e
Piotr Prokopowicz <git@nes.slmail.me>
2023-08-16 21:29:13 +0200

    Create snippet form added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,7 +20,7 @@ type application struct {
 }
 
 func main() {
-	addr := flag.String("addr", ":4000", "HTPP network address")
+	addr := flag.String("addr", ":4000", "HTTP network address")
 	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
 	flag.Parse()
