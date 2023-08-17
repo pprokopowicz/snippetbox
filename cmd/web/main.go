9b877412361e4eb466c63d342ca59a47bd4e5d24
Piotr Prokopowicz <git@nes.slmail.me>
2023-08-17 18:35:45 +0200

    Server upgraded to use TLS

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -50,6 +50,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		errorLog:       errorLog,
@@ -67,7 +68,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
