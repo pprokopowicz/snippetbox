b6e6e55445f9ecb173d6ac05bc8c42b7c93be881
Piotr Prokopowicz <git@nes.slmail.me>
2023-08-17 21:27:09 +0200

    Require authentication middleware implemented

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -13,8 +13,9 @@ import (
 
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
@@ -73,4 +74,8 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 	}
 
 	return nil
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
