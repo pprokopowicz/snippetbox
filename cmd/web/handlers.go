c020a2e111da8438b95a1fea91a139c066095bda
Piotr Prokopowicz <git@nes.slmail.me>
2023-08-17 21:16:56 +0200

    Logout implemented

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -208,5 +208,14 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user...")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serveError(w, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
