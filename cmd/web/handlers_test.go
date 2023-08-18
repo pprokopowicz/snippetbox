027164d3baf36797d4f6a88c0eb09acd0033f765
Piotr Prokopowicz <git@nes.slmail.me>
2023-08-18 17:42:36 +0200

    TestPing updated

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -11,28 +11,15 @@ import (
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := newTestApplication(t)
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	ping(rr, r)
+	code, _, body := ts.get(t, "/ping")
 
-	rs := rr.Result()
-
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	body = bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
 
 func TestSecureHeaders(t *testing.T) {
